aaef9a8b3b8a397220a4b938f55973570ed024bb
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2025-12-03 13:16:23 +0530

    convert to pdf and download utility function

---
+++ src/utility/pdfconvertor.js
@@ -0,0 +1,41 @@
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
+export default async function downloadInvoiceAsPDF(
+  contentRef,
+  clientName = ""
+) {
+  if (!contentRef.current) {
+    console.error("Invoice content reference not found.");
+    return;
+  }
+
+  const element = contentRef.current;
+
+  const canvas = await html2canvas(element, {
+    useCORS: true,
+    backgroundColor: null,
+  });
+
+  console.log(canvas, canvas.width);
+
+  const imgData = canvas.toDataURL("image/png");
+
+  const pdf = new jsPDF({
+    orientation: "portrait",
+    unit: "px",
+    format: [canvas.width, canvas.height],
+  });
+
+  pdf.addImage(imgData, "PNG", 0, 0, canvas.width, canvas.height);
+
+  const date = new Date();
+  const fileName =
+    clientName +
+    date.getFullYear().toString() +
+    date.getDay().toString() +
+    date.getMonth().toString() +
+    "_INVOICE";
+
+  pdf.save(fileName);
+}
